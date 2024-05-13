3ea8c45fa9220c4a35b44f08c77deabad4c7015c
GabriielND <gabriel_deodato@hotmail.com>
2024-05-13 13:37:55 -0300

    Correção na fase 13-05

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import './App.css';
 import {validarResposta} from './tmdbAPI.js';
 
-const versaoAtual = "12052024"
+const versaoAtual = "13052024"
 let tabuleiroTexto
 let textoShare
 let dataCompleta
