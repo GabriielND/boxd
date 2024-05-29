a7d5eff157db5c5fc838fa0cc844af7f2a95de82
GabriielND <gabriel_deodato@hotmail.com>
2024-05-29 00:05:22 -0300

    Correção de segurança

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import './App.css';
 import {validarResposta} from './tmdbAPI.js';
 
-const versaoAtual = "24052024"
+const versaoAtual = "29052024"
 const dataControle = ""
 let tabuleiroTexto
 let textoShare
