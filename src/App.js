0d9987712acd5eef097b98cad18f6a7f3c1ee646
GabriielND <gabriel_deodato@hotmail.com>
2024-06-21 23:23:41 -0300

    Correção

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import './App.css';
 import {validarResposta} from './tmdbAPI.js';
 
-const versaoAtual = "21062024"
+const versaoAtual = "20062024"
 const dataControle = ""
 let tabuleiroTexto
 let textoShare
