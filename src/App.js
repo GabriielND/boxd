48789132609bd236d475f32064aebe12cae409bb
GabriielND <85093699+GabriielND@users.noreply.github.com>
2024-10-05 15:10:00 -0300

    Erro em Nacional corrigido

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import './App.css';
 import {validarResposta} from './tmdbAPI.js';
 
-const versaoAtual = "310720241"
+const versaoAtual = "05102024"
 const dataControle = ""
 let tabuleiroTexto
 let textoShare
