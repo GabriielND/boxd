b0ded4cd118aa1753a7192e80ae41efc83bf9ca6
GabriielND <gabriel_deodato@hotmail.com>
2025-08-05 15:02:44 -0300

    Correção nas categorias novas

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import './App.css';
 import {setUrlPessoa, validarResposta} from './tmdbAPI.js';
 
-const versaoAtual = "07072025"
+const versaoAtual = "05082025"
 const dataControle = ""
 let tabuleiroTexto
 let textoShare
